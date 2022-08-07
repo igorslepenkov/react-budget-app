36581da1247a02080368c1dcdd34c8f2e793db1e
igorslepenkov <slepenkov.nii@yandex.by>
2022-08-07 23:55:55 +0300

    feat(hw_39): create Search component

---
+++ src/components/Search/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledSearch = styled.input`
+  width: 100%;
+  padding: 15px 20px;
+  outline: none;
+  border: none;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+`;
